3308b76c2cb559b45e90a7a06d53bb538f29fcea
yumnahayaaqilah <yumnahayaaqilah@gmail.com>
2026-03-12 22:45:56 +0700

    feat: add PersonalAccessToken.php

---
+++ backend/app/Models/PersonalAccessToken.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Laravel\Sanctum\PersonalAccessToken as SanctumToken;
+use MongoDB\Laravel\Eloquent\Model as MongoModel;
+
+class PersonalAccessToken extends SanctumToken
+{
+    protected $connection = 'mongodb';
+    protected $collection = 'personal_access_tokens';
+}
