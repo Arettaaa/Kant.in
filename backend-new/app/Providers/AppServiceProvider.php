374c4b041504785fdc97f6cd78aa3a487ea2d12b
Arettaaa <aretta557@gmail.com>
2026-03-26 18:07:26 +0700

    pindah ke docker buat fix mongodb

--- backend-new/app/Providers/AppServiceProvider.php
+++ backend-new/app/Providers/AppServiceProvider.php
@@ -3,8 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Laravel\Sanctum\Sanctum; // Tambahkan ini
-use App\Models\PersonalAccessToken; // Tambahkan ini
+use Laravel\Sanctum\Sanctum; 
+use App\Models\PersonalAccessToken; 
+use Illuminate\Support\Facades\URL; // 1. Tambahkan import ini di atas
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +16,11 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        // Settingan Sanctum kamu yang lama tetap biarkan ada
         Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
+
+        if (!app()->isLocal()) { // Atau pakai URL::forceScheme('https') saja langsung boleh
+            URL::forceScheme('https');
+        }
     }
 }
