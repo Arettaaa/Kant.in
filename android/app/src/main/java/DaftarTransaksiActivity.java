b64742f8a51b6187fd919f56876d83084373b832
Arettaaa <aretta557@gmail.com>
2026-04-25 22:57:54 +0700

    pelanggan

--- android/app/src/main/java/DaftarTransaksiActivity.java
+++ android/app/src/main/java/DaftarTransaksiActivity.java
@@ -12,17 +12,13 @@ public class DaftarTransaksiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Pastikan nama ini sesuai dengan nama file XML tampilanmu
         setContentView(R.layout.bottom_sheet_export);
 
-        // Deklarasi dan Inisialisasi dijadikan satu (menyelesaikan warning local variable)
         ImageView btnDownloadLaporan = findViewById(R.id.btnDownload);
         ImageButton btnBack = findViewById(R.id.btnClose);
 
-        // Aksi Tombol Kembali
         btnBack.setOnClickListener(v -> finish());
 
-        // Aksi Tombol Unduh Laporan
         btnDownloadLaporan.setOnClickListener(v -> {
             ExportBottomSheetDialog bottomSheet = new ExportBottomSheetDialog();
             bottomSheet.show(getSupportFragmentManager(), "ExportBottomSheetDialog");
